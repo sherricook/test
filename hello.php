4d3227cb4b4bc414405259cbf8ce789e934c7fc3
SherriCook <sherri.e.cook@comcast.net>
2016-02-20 12:53:22 -0800

    hello

--- hello.php
+++ hello.php
@@ -4,7 +4,7 @@
  * Date: 1/2/2016
  * Description: Test Git
  */
-echo "Hello World Everyone!";
+echo "Hello World!";
 echo "<br> by Sherri Cook";
 echo "<br> from Beaverton";
 echo "<br> some more stuff";
