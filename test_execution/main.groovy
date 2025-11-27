b676addf08b2badf39f086a2394c877032e3cb07
Sherri Cook <sherri.e.cook@comcast.net>
2025-11-27 04:42:46 -0800

    add cleanup

--- test_execution/main.groovy
+++ test_execution/main.groovy
@@ -5,6 +5,14 @@ def call(config) {
         echo "Provision Windows? ${config.provision_win}"
         echo "Credentials: ${config.credentials}"
     }
+	
+	stage('Build') {
+		// Clean workspace before build
+		cleanWs()
+		// Explicitly checkout source code
+		checkout scm
+		echo "Building ${env.JOB_NAME}..."
+	}
 }
 
 return this
